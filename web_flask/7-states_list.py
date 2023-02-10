4765c2ee16d5f4530bce6e3a947423bae19e048b
dayo2013 <emmanuelgbaje501@gmail.com>
2023-02-10 14:27:33 -0800

    7. Improve engines

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Falsk web app, fetching data from storage and using template
+"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """Return states list
+    """
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    """Clean-up session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
